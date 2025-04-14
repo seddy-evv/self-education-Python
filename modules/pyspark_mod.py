dd19ba2fb0ecd87aeb8de4bdfc515703058d5fd6
Anatol <seddboom@gmail.com>
2025-04-14 10:07:29 +0300

    Update pyspark_mod.py

--- modules/pyspark_mod.py
+++ modules/pyspark_mod.py
@@ -2,7 +2,9 @@
 # It allows handling large-scale data across multiple nodes and provides a rich set of functions for data processing.
 # Below is a description and examples of some of the main PySpark functions:
 
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, Row
+from pyspark.sql.functions import col, when, sum, max, concat, lit, expr
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType
 
 # Create SparkSession
 spark = SparkSession.builder.appName("PySpark Examples").master("local").getOrCreate()
@@ -86,3 +88,282 @@ rdd10 = spark.sparkContext.parallelize([("a", 1), ("b", 4)])
 rdd11 = spark.sparkContext.parallelize([("a", 2), ("a", 3)])
 join_rdd = rdd10.join(rdd11)
 print(join_rdd.collect())  # [("a", (1, 2)), ("a", (1, 3))]
+
+
+# Create DataFrame from Python List of Tuples
+
+data = [("Alice", 28), ("Bob", 25), ("Charlie", 30)]
+df = spark.createDataFrame(data, schema=["Name", "Age"])
+df.show()
+# +-------+---+
+# |   Name|Age|
+# +-------+---+
+# |  Alice| 28|
+# |    Bob| 25|
+# |Charlie| 30|
+# +-------+---+
+
+# Create DataFrame from schema with types
+schema = StructType([
+    StructField("Name", StringType(), True),
+    StructField("Age", IntegerType(), True)
+])
+data = [("Alice", 28), ("Bob", 25), ("Charlie", 30)]
+df = spark.createDataFrame(data, schema=schema)
+df.show()
+# +-------+---+
+# |   Name|Age|
+# +-------+---+
+# |  Alice| 28|
+# |    Bob| 25|
+# |Charlie| 30|
+# +-------+---+
+
+# Additional methods to create DataFrame
+# from a Pandas DataFrame
+import pandas as pd
+pandas_df = pd.DataFrame({"Name": ["Alice", "Bob", "Charlie"], "Age": [28, 25, 30]})
+df = spark.createDataFrame(pandas_df)
+df.show()
+# +-------+---+
+# |   Name|Age|
+# +-------+---+
+# |  Alice| 28|
+# |    Bob| 25|
+# |Charlie| 30|
+# +-------+---+
+
+# from an RDD
+rdd = spark.sparkContext.parallelize([("Alice", 28), ("Bob", 25), ("Charlie", 30)])
+df = rdd.toDF(["Name", "Age"])
+df.show()
+# +-------+---+
+# |   Name|Age|
+# +-------+---+
+# |  Alice| 28|
+# |    Bob| 25|
+# |Charlie| 30|
+# +-------+---+
+
+# from a list of Row objects
+data = [Row(Name="Alice", Age=28), Row(Name="Bob", Age=25), Row(Name="Charlie", Age=30)]
+df = spark.createDataFrame(data)
+df.show()
+# +-------+---+
+# |   Name|Age|
+# +-------+---+
+# |  Alice| 28|
+# |    Bob| 25|
+# |Charlie| 30|
+# +-------+---+
+
+
+# Basic PySpark DataFrame Operations
+
+data = [("Alice", 28), ("Bob", 25), ("Charlie", 30)]
+df = spark.createDataFrame(data, schema=["Name", "Age"])
+
+# Collect: Retrieve all data
+data = df.collect()
+print(data)
+# [Row(Name='Alice', Age=28), Row(Name='Bob', Age=25), Row(Name='Charlie', Age=30)]
+
+# Count: Count the number of rows
+count = df.count()
+print(count)
+# 3
+
+# Show: Display the DataFrame
+df.show()
+# +-------+---+
+# |   Name|Age|
+# +-------+---+
+# |  Alice| 28|
+# |    Bob| 25|
+# |Charlie| 30|
+# +-------+---+
+
+# Select Columns
+# We can specify the colum name in two ways
+df.select("Name").show()
+# or
+df.select(df.Name).show()
+# +-------+
+# |   Name|
+# +-------+
+# |  Alice|
+# |    Bob|
+# |Charlie|
+# +-------+
+
+# Filter Rows
+df.filter(df.Age > 26).show()
+# +-------+---+
+# |   Name|Age|
+# +-------+---+
+# |  Alice| 28|
+# |Charlie| 30|
+# +-------+---+
+
+# Add New Column
+df.withColumn("AgePlusOne", col("Age") + lit(1)).show()
+# +-------+---+----------+
+# |   Name|Age|AgePlusOne|
+# +-------+---+----------+
+# |  Alice| 28|        29|
+# |    Bob| 25|        26|
+# |Charlie| 30|        31|
+# +-------+---+----------+
+
+# Replace Column Values
+df.withColumnRenamed("Name", "New Name").show()
+# +--------+---+
+# |New Name|Age|
+# +--------+---+
+# |   Alice| 28|
+# |     Bob| 25|
+# | Charlie| 30|
+# +--------+---+
+
+# GroupBy and Aggregate
+df.groupBy("Age").count().show()
+# +---+-----+
+# |Age|count|
+# +---+-----+
+# | 30|    1|
+# | 25|    1|
+# | 28|    1|
+# +---+-----+
+
+data = [("Alice", 2000), ("Bob", 3000), ("Charlie", 4000), ("Bob", 4000)]
+df3 = spark.createDataFrame(data, schema=["Name", "Salary"])
+aggregated_df = df3.groupBy("Name").agg(sum("Salary").alias("Salary sum"), max("Salary"))
+aggregated_df.show()
+# +-------+----------+-----------+
+# |   Name|Salary sum|max(Salary)|
+# +-------+----------+-----------+
+# |  Alice|      2000|       2000|
+# |    Bob|      7000|       4000|
+# |Charlie|      4000|       4000|
+# +-------+----------+-----------+
+
+# Drop columns
+data = [("Alice", 28, "Amazon"), ("Bob", 25, "Google"), ("Charlie", 30, "Oracle")]
+df1 = spark.createDataFrame(data, schema=["Name", "Age", "Company"])
+df1.drop("Name", "Age").show()
+# +-------+
+# |Company|
+# +-------+
+# | Amazon|
+# | Google|
+# | Oracle|
+# +-------+
+
+# Limit rows
+df1.limit(1).show()
+# +-----+---+-------+
+# | Name|Age|Company|
+# +-----+---+-------+
+# |Alice| 28| Amazon|
+# +-----+---+-------+
+
+# Union
+data = [("Alex", 38, "Microsoft"), ("John", 35, "Netflix")]
+df2 = spark.createDataFrame(data, schema=["Name", "Age", "Company"])
+df1.union(df2).show()
+# +-------+---+---------+
+# |   Name|Age|  Company|
+# +-------+---+---------+
+# |  Alice| 28|   Amazon|
+# |    Bob| 25|   Google|
+# |Charlie| 30|   Oracle|
+# |   Alex| 38|Microsoft|
+# |   John| 35|  Netflix|
+# +-------+---+---------+
+
+
+# PySpark SQL
+
+# Register Temp View and Execute SQL
+df.createOrReplaceTempView("people")
+spark.sql("SELECT Name, Age FROM people WHERE Age > 26").show()
+# +-------+---+
+# |   Name|Age|
+# +-------+---+
+# |  Alice| 28|
+# |Charlie| 30|
+# +-------+---+
+
+
+# PySpark DataFrame Joins
+
+data1 = [("Alice", 28), ("Bob", 25)]
+data2 = [("Alice", "F"), ("Bob", "M"), ("Charlie", "M")]
+df1 = spark.createDataFrame(data1, ["Name", "Age"])
+df2 = spark.createDataFrame(data2, ["Name", "Gender"])
+
+# Inner Join
+inner_join = df1.join(df2, on="Name", how="inner")
+inner_join.show()
+# +-----+---+------+
+# | Name|Age|Gender|
+# +-----+---+------+
+# |Alice| 28|     F|
+# |  Bob| 25|     M|
+# +-----+---+------+
+
+# Left Join
+left_join = df2.join(df1, on="Name", how="left")
+left_join.show()
+# +-------+------+----+
+# |   Name|Gender| Age|
+# +-------+------+----+
+# |  Alice|     F|  28|
+# |    Bob|     M|  25|
+# |Charlie|     M|null|
+# +-------+------+----+
+
+
+# PySpark File I/O
+
+# Write DataFrame as CSV
+df.write.csv("output.csv", header=True)
+
+# Read CSV File
+df = spark.read.csv("output.csv", header=True, inferSchema=True)
+df.show()
+
+# Read and Write Parquet
+df.write.parquet("output.parquet")
+df_parquet = spark.read.parquet("output.parquet")
+df_parquet.show()
+
+
+# PySpark Functions Module
+
+# concat() - concatenates multiple input columns together into a single column
+# lit() - creates a Column of literal value. Often used with withColumn() expression,
+# because it requires a Column type parameter.
+# col() - returns a Column(type) based on the given column name
+df.withColumn("FullMessage", concat(col("Name"), lit(" is "), col("Age"), lit(" years old."))).show()
+# +-------+---+--------------------+
+# |   Name|Age|        FullMessage|
+# +-------+---+--------------------+
+# |  Alice| 28|Alice is 28 years...|
+# |    Bob| 25|  Bob is 25 years...|
+# |Charlie| 30|Charlie is 30 yea...|
+# +-------+---+--------------------+
+
+# when() - Evaluates a list of conditions and returns one of multiple possible result expressions.
+# If pyspark.sql.Column.otherwise() is not invoked, None is returned for unmatched conditions.
+df.withColumn("Name", when(df.Name == "Alice", "Alicia").otherwise(df.Name)).show()
+# +-------+---+
+# |   Name|Age|
+# +-------+---+
+# | Alicia| 28|
+# |    Bob| 25|
+# |Charlie| 30|
+# +-------+---+
+
+# expr() - Parses the expression string into the column that it represents. Expression defined in string.
+df.select("name", expr("length(name)").alias("lenght of the name")).show()
