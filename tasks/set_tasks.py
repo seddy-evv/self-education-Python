65f17eb56cda7c37d396637bb6cfb68ab6c83f20
Anatol <seddboom@gmail.com>
2025-06-28 22:32:16 +0300

    Update set_tasks.py

--- tasks/set_tasks.py
+++ tasks/set_tasks.py
@@ -1,8 +1,15 @@
 # The function takes two lists of numbers and counts how many distinct numbers
-# are in the first and second lists at the same time.
-
-
+# are in the first and second lists in total.
 def count_numbers(list1, list2):
+    set1 = set(list1)
+    set2 = set(list2)
+    count_of_numbers = len(set1 | set2)
+    print(count_of_numbers)
+
+
+# The function takes two lists of numbers and counts how many distinct numbers
+# are included in the first and second lists at the same time.
+def count_numbers1(list1, list2):
     set1 = set(list1)
     set2 = set(list2)
     count_of_numbers = len(set1 & set2)
@@ -10,10 +17,17 @@ def count_numbers(list1, list2):
 
 
 # The function takes two lists of numbers and counts how many distinct numbers
-# are in the first and the second list.
+# are included in just one of these lists.
+def count_numbers2(list1, list2):
+    set1 = set(list1)
+    set2 = set(list2)
+    count_of_numbers = len(set1 - set2)
+    print(count_of_numbers)
 
 
-def count_numbers_first(list1, list2):
+# The function takes two lists of numbers and counts how many distinct numbers
+# are in the first and the second list except for those found in both lists.
+def count_numbers3(list1, list2):
     set1 = set(list1)
     set2 = set(list2)
     count_of_numbers = len(set1 ^ set2)
@@ -22,20 +36,15 @@ def count_numbers_first(list1, list2):
 
 # The function takes the text and counts the number of
 # distinct words separated by spaces and end-of-line characters.
-
-
 def count_different_words(text):
     print(len({sentence for sentence in text.split()}))
 
 
-rand_text = """Walking, running, cycling and and
-              playing football are some kinds
-              of sports that     you do every day day"""
-
-
 if __name__ == '__main__':
-    count_numbers([1, 3, 4, 1, 5, 7], [7, 8, 3, 22, 1, 18])
-    count_numbers_first([1, 3, 4, 1, 5, 7], [7, 8, 3, 22, 1, 18])
+    count_numbers1([1, 3, 4, 1, 5, 7], [7, 8, 3, 22, 1, 18])
+    count_numbers1([1, 3, 4, 1, 5, 7], [7, 8, 3, 22, 1, 18])
+    count_numbers2([1, 3, 4, 1, 5, 7], [7, 8, 3, 22, 1, 18])
+    count_numbers3([1, 3, 4, 1, 5, 7], [7, 8, 3, 22, 1, 18])
     rand_text = """Walking, running, cycling and and
                   playing football are some kinds
                   of sports that     you do every day day"""
