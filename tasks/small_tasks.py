881537a5610572ee521f66027c53a539c137b98e
Anatol <seddboom@gmail.com>
2025-06-28 21:20:29 +0300

    Update small_tasks.py

--- tasks/small_tasks.py
+++ tasks/small_tasks.py
@@ -1,16 +1,17 @@
-# functions
+"""functions"""
 def sum_calc(a_atr, b_atr):
     return a_atr + b_atr
 
-
 print(sum_calc(1, 2))
+# 3
 
 lambda_func = lambda x, y: x + y
 
 print(lambda_func(3, 2))
+# 5
 
 
-# exceptions
+"""exceptions"""
 try:
     b = 1/0
 except Exception as exp:
@@ -19,34 +20,38 @@ else:
     pass
 finally:
     pass
+# exception division by zero
 
 
-# str functions
+"""str functions"""
 def str_sepr(exmp):
     words = exmp.split()
     out = ".".join(words)
     print(out)
 
-
 str_sepr("My name is Anatol")
+# My.name.is.Anatol
 
 
-# add 1 to all elements
+"""add 1 to all elements"""
 lst = [1, 2, 3]
 
 for i in enumerate(lst):
     lst[i[0]] = lst[i[0]] + 1
 print(lst)
+# [2, 3, 4]
 
 lst = [i + 1 for i in lst]
 print(lst)
+# [3, 4, 5]
 
 for i in range(len(lst)):
     lst[i] = lst[i] + 1
 print(lst)
+# [4, 5, 6]
 
 
-# decorator
+"""decorator"""
 def dec_func(func):
     # some code
     def wrapper(*args, **kwargs):
@@ -56,32 +61,32 @@ def dec_func(func):
         return result
     return wrapper
 
-
 @dec_func
 def my_func():
     return "hi"
 
-
 my_func()
+# 123
+# hi
 
 
-# open files
+"""open files"""
 try:
     with open("file", "prop") as fh:
         for line in fh:
             print(line)
 except:
     print("no file")
+# no file
 
-
-# reassign variables
+"""reassign variables"""
 a = 1
 b = 2
 
 a, b = b, a
 
 
-# palindrome
+"""palindrome"""
 def palindrome(origin_number):
     total_number = 0
     num = origin_number
@@ -94,12 +99,13 @@ def palindrome(origin_number):
     else:
         return False
 
-
 print(palindrome(187781))
+# True
 print(palindrome(187981))
+# False
 
 
-# move zeros to the end
+"""move zeros to the end"""
 ex = [1, 0, 2, 0, 3]
 
 for i in range(len(ex)):
@@ -108,51 +114,72 @@ for i in range(len(ex)):
         ex.pop(i)
         ex.append(a)
 print(ex)
+# [1, 2, 3, 0, 0]
 
 
-# back slice
+"""print duplicates"""
+lst_ = [1, 2, 4, 6, 2, 1, 3, 2]
+def print_duplicates(lst):
+    duplicates = {}
+    for n in lst:
+        duplicates[n] = duplicates.get(n, 0) + 1
+    for key, value in duplicates.items():
+        if value > 1:
+            print(key)
+
+print_duplicates(lst_)
+# 1
+# 2
+
+
+"""back slice"""
 ex = [1, 0, 2, 0, 3]
 print(ex[-1:2:-1])
+# [3, 0]
 
 
-# get file format
+"""get file format"""
 name = "asamples-workitemstest7.jpg"
 paths = name.split(".")
 print(paths[-1])
+# jpg
 
 
-# Remove all values from a list present in another list – three approaches
+"""Remove all values from a list present in another list – three approaches"""
 
-# First
+"""First"""
 a = [1, 2, 3, 4, 5]
 b = [2, 4]
 
 result = list(set(a) - set(b))
 print(result)
+# [1, 3, 5]
 
-# Second
+"""Second"""
 a = [1, 2, 3, 4, 5]
 b = [2, 4]
 
 result = [item for item in a if item not in b]
 print(result)
+# [1, 3, 5]
 
-# Third
+"""Third"""
 a = [1, 2, 3, 4, 5]
 b = [2, 4]
-
 for item in b:
     while item in a:
         a.remove(item)
 print(a)
+# [1, 3, 5]
 
 
-# hash task
+"""hash task"""
 data = {1, True, 1.0}
 print(data)
 # {1}
 
-# Exaplanation:
+
+"""Exaplanation:"""
 print(hash(1))
 print(hash(True))
 print(hash(1.0))
@@ -161,7 +188,7 @@ print(hash(1.0))
 # 1
 
 
-# tuple with list task
+"""tuple with list task"""
 data = (['bee'], 'pygen')
 try:
     data[0] = data[0] + ['geek']
@@ -173,40 +200,42 @@ except Exception as exp:
 # print(data)
 # TypeError: 'tuple' object does not support item assignment
 
-# but it works
+"""but it works"""
 data = (['bee'], 'pygen')
 data[0].append(['geek'])
 print(data)
 # (['bee', ['geek']], 'pygen')
 
 
-# dict keys
+"""dict keys"""
 dct = {None: 0, True: 1, False: 0, (0, 1): 1}
 print(dct)
 # {None: 0, True: 1, False: 0, (0, 1): 1}
 
 try:
     dct = {(0, [1]): 1}
-except:
-    print(dct)
+except Exception as exp:
+    print(exp)
     # TypeError: unhashable type: 'list'
 
 
-# infinite recursion
+"""infinite recursion"""
 a = []
 a.append(a)
 print(a)
 # [[...]]
 
 
-# random
+"""random"""
 import random
 
 # random(): Returns a random floating-point number between 0.0 (inclusive) and 1.0 (exclusive).
 number = random.random()
 print(number)
-# 0.3848767341685718
+# 0.07761683093797356
 print(number * 100)
+# 7.761683093797355
+
 # randint() takes two arguments, a and b (includes both the start and end values), representing the start
 # and end of the range, respectively.
 # It is an alias for randrange(a, b + 1, 1).
