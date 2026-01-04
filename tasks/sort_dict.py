1a8c9ff1b5250065e36f97d0e50df147d6fcf3c0
Anatol <seddboom@gmail.com>
2026-01-04 17:38:27 +0300

    Update sort_dict.py

--- tasks/sort_dict.py
+++ tasks/sort_dict.py
@@ -20,3 +20,19 @@ import operator
 # # Sort the dictionary by value using itemgetter
 sorted_data_asc = dict(sorted(initial_dict.items(), key=operator.itemgetter(1)))
 
+
+# Older Python versions
+sorted_data_asc = sorted(initial_dict.items(), key=lambda kv: kv[1])
+print(sorted_data_asc)
+# [('a', 1), ('b', 5), ('c', 10), ('d', 12)]
+
+# Custom logic
+
+tuples = list(initial_dict.items())
+n = len(tuples)
+for bypass in range(1, n):
+    for k in range(0, n-bypass):
+        if tuples[k][1] > tuples[k + 1][1]:
+            tuples[k], tuples[k + 1] = tuples[k + 1], tuples[k]
+print(dict(tuples))
+# {'a': 1, 'b': 5, 'c': 10, 'd': 12}
