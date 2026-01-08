2ca27ec43fb46b6bc53e2e72e55bcff21b74a082
Anatol <seddboom@gmail.com>
2026-01-08 13:09:36 +0300

    Update round_0_0_5.py

--- tasks/round_0_0_5.py
+++ tasks/round_0_0_5.py
@@ -1,34 +1,29 @@
-# 1. In Python 3, the round() function uses a method called "round half to even" or banker's rounding. This means that
-# when a number is exactly halfway between two integers (e.g., ends in .5), it is rounded to the nearest even integer.
-
+"""1. In Python 3, the round() function uses a method called "round half to even" or banker's rounding. This means that
+when a number is exactly halfway between two integers (e.g., ends in .5), it is rounded to the nearest even integer."""
 print(round(2.5))
 # 2
 print(round(3.5))
 # 4
 
 
-# 2. Rounds to two decimal places.
-
+""" 2. Rounds to two decimal places."""
 print(round(3.14159, 2))
 # 3.14
 
 
-# 3. Rounds to the nearest hundred (left of the decimal).
-
+""" 3. Rounds to the nearest hundred (left of the decimal)."""
 print(round(12345, -2))
 # 12300
 
 
-# 4. Round using basic math rules
-# 4.1 The easiest way is to add 0.01
-
+""" 4. Round using basic math rules"""
+""" 4.1 The easiest way is to add 0.01"""
 print(round(2.5 + 0.01))
 # 3
 print(round(3.5 + 0.01))
 # 4
 
-# 4.2 Custom function
-
+""" 4.2 Custom function"""
 from decimal import Decimal, ROUND_HALF_UP
 
 def traditional_round(number, ndigits=0):
@@ -53,8 +48,7 @@ print(traditional_round(-2.55, 1))
 # -2.6
 
 
-# 5. How to round to 0.05 or custom value
-
+""" 5. How to round to 0.05 or custom value"""
 number = 1.27
 rounded_number = round(number / 0.05) * 0.05
 print(rounded_number)
