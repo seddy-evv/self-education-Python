e745be1ec9b52770f3f4609a2ad15fc9e2328ba5
Anatol <seddboom@gmail.com>
2026-01-28 15:01:05 +0300

    Update small_tasks.py

--- tasks/small_tasks.py
+++ tasks/small_tasks.py
@@ -317,5 +317,20 @@ with nullcontext():
     print("Inside a dummy context manager")
 
 
+"""match-case"""
+def http_error(status):
+    match status:
+        case 400:
+            return "Bad request"
+        case 404:
+            return "Not found"
+        case 418:
+            return "I'm a teapot"
+        case _:
+            return "Something's wrong with the internet"
+
+print(http_error(400))
+
+
 """assert without unit tests"""
 assert 1 == 2, "error message"
